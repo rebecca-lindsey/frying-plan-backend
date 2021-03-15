4bac8a4d4b25f5d8af3de94431c9cf7760e39e7d
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-15 10:10:26 -0400

    Correct Recipe migration and add seed file

--- db/migrate/20210315140747_create_recipes.rb
+++ db/migrate/20210315140747_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.string :type
+      t.string :category
       t.string :cuisine
       t.text :ingredients
       t.text :instructions
