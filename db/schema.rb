e8a5dfacce8b3721b9fd229da652ec1c502fe419
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-15 10:10:26 -0400

    Correct Recipe migration and add seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_135129) do
+ActiveRecord::Schema.define(version: 2021_03_15_140747) do
 
   create_table "days", force: :cascade do |t|
     t.string "name"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_135129) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "category"
     t.string "cuisine"
     t.text "ingredients"
     t.text "instructions"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: 'Rebecca')
+
+Day.create(name: 'Monday', date: Date.new(2021, 0o3, 22), user_id: 1)
+Day.create(name: 'Tuesday', date: Date.new(2021, 0o3, 23), user_id: 1)
+Day.create(name: 'Wednesday', date: Date.new(2021, 0o3, 24), user_id: 1)
+Day.create(name: 'Thursday', date: Date.new(2021, 0o3, 25), user_id: 1)
+Day.create(name: 'Friday', date: Date.new(2021, 0o3, 26), user_id: 1)
+Day.create(name: 'Saturday', date: Date.new(2021, 0o3, 27), user_id: 1)
+Day.create(name: 'Sunday', date: Date.new(2021, 0o3, 28), user_id: 1)
+
+Meal.create(name: 'Breakfast', day_id: 1, recipe_id: 1)
+
+Recipe.create(name: 'Buttered Toast', category: 'Breakfast', cuisine: 'American', ingredients: [{ name: 'Bread', amount: '2 slices' }, { name: 'Butter', amount: '2 pats' }], instructions: 'Put the bread in the toaster, when done, add butter')
