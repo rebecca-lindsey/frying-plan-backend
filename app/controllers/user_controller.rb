65e7b1307792fa9df68a61e0b5a561ada4f7dad1
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-15 11:36:52 -0400

    Add show route and clean up routes.rb

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,4 +3,9 @@ class UserController < ApplicationController
     users = User.all
     render json: users, include: ['days', 'days.meals', 'days.meals.recipe']
   end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, include: ['days', 'days.meals', 'days.meals.recipe']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'recipe/index'
-  get 'meal/index'
-  get 'user/index'
-  get 'day/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :user, only: %i[index show]
+  resources :day, only: %i[index]
+  resources :meal, only: %i[index]
+  resources :route, only: %i[index]
 end
