a3b98e5311c8330bb0e2e082b4aecf06b971c917
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-15 10:13:49 -0400

    Add controllers for all models

---
+++ app/controllers/day_controller.rb
@@ -0,0 +1,4 @@
+class DayController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'recipe/index'
+  get 'meal/index'
+  get 'user/index'
+  get 'day/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
