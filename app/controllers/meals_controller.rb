aa8b8aa2862f4b83f5608a3c12ffb4448ea716c4
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-20 12:01:57 -0400

    Set up DB to receive new meals

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,4 +1,15 @@
 class MealsController < ApplicationController
   def index
+    meals = Meal.all
+    render json: meals
+  end
+
+  def create
+    meal = Meal.create(meal_params)
+    render json: meal
+  end
+
+  def meal_params
+    params.require(:meal).permit(:name, :recipe_id, :day_id)
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,10 +9,6 @@ class RecipesController < ApplicationController
     render json: recipe
   end
 
-  def show
-    byebug
-  end
-
   def recipe_params
     params.require(:recipe).permit(:name, :category, :cuisine, :instructions, recipe_ingredients_attributes: [:amount, { ingredient_attributes: :name }])
   end
